4b681bd1e94e2b7c84442a0c2034d5a183e1945c
infsolution <clsinfsolution@gmail.com>
2020-06-29 21:22:14 -0300

    add panel, login, logout, views methods

--- onepage/urls.py
+++ onepage/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from onepage import views
 urlpatterns = [
-    path('recrutamento/', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('panel/',views.panel, name='panel'),
     path('update/', views.update, name='update'),
+    path('login/',views.do_login, name='login'),
+    path('logout/',views.do_logout, name='logout'),
 ]
--- onepage/views.py
+++ onepage/views.py
@@ -1,11 +1,32 @@
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 def index(request):
 	return render(request, 'onepage/index.html')
+
+def panel(request):
+	user = User.objects.get(id=request.user.id)
+	return render(request, 'onepage/panel.html',{'user':user})
 
 @login_required
 def update(request):
 	if request.method == 'POST':
 		pass
 	return render(request, 'onepage/form.html')
+
+
+def do_login(request):
+	if request.method == 'POST':
+		user = authenticate(username = request.POST['username'], password =  request.POST['password'])
+		print(user)
+		if user is not None:
+			login(request,user)
+			return redirect('/panel',user)
+		else:
+			return render(request, 'onepage/login.html', {'error':'Usuario ou senha inválidos'})
+	return render(request, 'onepage/login.html')
+
+def do_logout(request):
+	logout(request)
+	return redirect('/login')
