ec73bdbda4f530045949d63b2b410ef3bc2c6656
infsolution <clsinfsolution@gmail.com>
2020-07-16 09:17:51 -0300

    update settings

--- onepagemt/wsgi.py
+++ onepagemt/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import  time
+import traceback
+import signal
+import sys
 
 from django.core.wsgi import get_wsgi_application
+sys.path.append('/var/www/html/onepagemt')
+sys.path.append('/var/www/html/venvmt/lib/python3.5/site-packages')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onepagemt.settings')
-
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except Exception:
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
