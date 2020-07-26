05949868aaa8a9497d236fc371a448f0469775fc
infsolution <clsinfsolution@gmail.com>
2020-07-26 19:41:26 +0000

    update cache

--- onepagemt/settings.py
+++ onepagemt/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^bz^$_v5hfv8-w%f#601xz%c83mrc_$)kp3c#@f)jljn@42ksv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
